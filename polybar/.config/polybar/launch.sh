25606a0487d784f2dd9e1e3cc27df13b614a71c8
Craig Cabrey <craigcabrey@gmail.com>
2017-01-23 21:30:38 -0800

    use host specific environment variables in polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,10 +1,21 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+# Find where we are for later use
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Set host specific environment variables
+source "$DIR/`hostname`"
+
+if [[ ! -v WLAN_INTERFACE ]]; then
+    # Fallback to wlp3s0 since it's so far the most common
+    export WLAN_INTERFACE="wlp3s0"
+fi
 
 polybar top &
 
